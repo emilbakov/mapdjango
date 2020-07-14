84f2b4c7ea98b3e40cbbe548be5a59b9a3a0af96
unknown <eb.bakov@gmail.com>
2020-07-14 13:18:16 +0300

    baseapp

---
+++ map/admin.py
@@ -0,0 +1,7 @@
+from leaflet.admin import LeafletGeoAdmin
+from django.contrib import admin
+
+from . import models as hunting_models
+
+
+admin.site.register(hunting_models.HuntingSpot, LeafletGeoAdmin)
---
+++ map/models.py
@@ -0,0 +1,18 @@
+from djgeojson.fields import PolygonField
+from django.db import models
+
+class HuntingSpot(models.Model):
+
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    picture = models.ImageField()
+    geom = PolygonField()
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def picture_url(self):
+        return self.picture.url
+
+# Create your models here.
